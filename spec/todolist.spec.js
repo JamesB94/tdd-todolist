9bf6f28f18e67af2f175ed63c9bc565f6e768f94
James Byrne <james.c.byrne1994@gmail.com>
2023-01-25 23:07:00 +0000

    core criteria met

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,108 @@
+const ToDoList = require('../src/todolist.js')
 
+describe('ToDoList', () => {
+  it('create obj with 3 keys, id, text and status(starts incomplete)', () => {
+    const newObj = new ToDoList()
+    expect(newObj.create('task 1')).toEqual({
+      id: 1,
+      text: 'task 1',
+      status: 'incomplete'
+    })
+  })
+
+  it('Get all todo items)', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    newObj.create('task 3')
+    expect(newObj.getAll('task 1')).toEqual([
+      {
+        id: 1,
+        text: 'task 1',
+        status: 'incomplete'
+      },
+      {
+        id: 2,
+        text: 'task 2',
+        status: 'incomplete'
+      },
+      {
+        id: 3,
+        text: 'task 3',
+        status: 'incomplete'
+      }
+    ])
+  })
+
+  it('Set a todo completed by its ID', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    expect(newObj.setToComplete(1)).toEqual({
+      id: 1,
+      text: 'task 1',
+      status: 'complete'
+    })
+  })
+
+  it('Get only todo items that are incomplete', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    newObj.setToComplete(2)
+    expect(newObj.getIncompleteItems(1)).toEqual({
+      id: 1,
+      text: 'task 1',
+      status: 'incomplete'
+    })
+  })
+
+  it('Get only todo items that are complete', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    newObj.setToComplete(2)
+    expect(newObj.getCompleteItems(1)).toEqual({
+      id: 2,
+      text: 'task 2',
+      status: 'complete'
+    })
+  })
+
+  it('Search and return a todo item by its ID', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    expect(newObj.searchById(1)).toEqual({
+      id: 1,
+      text: 'task 1',
+      status: 'incomplete'
+    })
+  })
+
+  it('return a message saying it doesnt exist', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    expect(newObj.searchById(3)).toEqual('does not exist')
+  })
+
+  it('Remove a todo item by its ID', () => {
+    const newObj = new ToDoList()
+    newObj.create('task 1')
+    newObj.create('task 2')
+    newObj.create('task 3')
+    newObj.removeById(2)
+    expect(newObj.getAll()).toEqual([
+      {
+        id: 1,
+        text: 'task 1',
+        status: 'incomplete'
+      },
+      {
+        id: 3,
+        text: 'task 3',
+        status: 'incomplete'
+      }
+    ])
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,99 @@
+// Create a todo item that has an ID, text description, and starts off incomplete
+// Get all todo items
+// Set a todo completed by its ID
+// Get only todo items that are incomplete
+// Get only todo items that are complete
+// Search and return a todo item by its ID, or return a message saying it doesn’t exist
+// Remove a todo item by its ID
 
+class ToDoList {
+  constructor() {
+    this.toDOArray = []
+  }
+
+  create(text) {
+    const newObj = {
+      id: this.toDOArray.length + 1,
+      text: text,
+      status: 'incomplete'
+    }
+    this.toDOArray.push(newObj)
+    console.log(newObj)
+    return newObj
+  }
+
+  getAll() {
+    console.log('this is getAll', this.toDOArray)
+    return this.toDOArray
+  }
+
+  setToComplete(id) {
+    let tobe = {}
+    this.toDOArray.forEach((i) => {
+      if (i.id === id) {
+        i.status = 'complete'
+        // console.log(i)
+        // console.log('type', typeof i)
+        tobe = i
+      }
+    })
+    return tobe
+    //console.log('here is tobe', tobe)
+  }
+
+  getIncompleteItems() {
+    let toBe2 = {}
+    this.toDOArray.forEach((i) => {
+      if (i.status === 'incomplete') {
+        console.log(i)
+        toBe2 = i
+      }
+    })
+    return toBe2
+  }
+
+  getCompleteItems() {
+    let toBe3 = {}
+    this.toDOArray.forEach((i) => {
+      if (i.status === 'complete') {
+        console.log(i)
+        toBe3 = i
+      }
+    })
+    return toBe3
+  }
+
+  searchById(id) {
+    let newObj = {}
+    this.toDOArray.forEach((i) => {
+      if (i.id === id) {
+        newObj = i
+      }
+    })
+    if (Object.keys(newObj).length === 0) {
+      console.log('does not exist')
+      return 'does not exist'
+    } else {
+      console.log(newObj)
+      return newObj
+    }
+  }
+
+  removeById(id) {
+    for (let i = 0; i < this.toDOArray.length; i++) {
+      if (this.toDOArray[i].id === id) {
+        this.toDOArray.splice(i, 1)
+      }
+    }
+    console.log(this.toDOArray)
+  }
+}
+
+module.exports = ToDoList
+
+const newToDo = new ToDoList()
+newToDo.create('Task 1')
+newToDo.create('Task 2')
+let test = newToDo.setToComplete(1)
+console.log('here is test', test)
+// newToDo.removeById(2)
